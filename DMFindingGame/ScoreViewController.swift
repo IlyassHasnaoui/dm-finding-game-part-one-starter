66aa633098c5332d34d13e7053c1febe045398c8
Ilyass Hasnaoui <123610021+IlyassHasnaoui@users.noreply.github.com>
2024-01-04 17:40:25 -0500

    New update

--- DMFindingGame/ScoreViewController.swift
+++ DMFindingGame/ScoreViewController.swift
@@ -7,77 +7,43 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+class ScoreViewController: UIViewController {
     
-    var timer: Timer!
-    let gameBrain = GameBrain.shared
     
-    @IBOutlet weak var targetLetter: UILabel!
-    @IBOutlet weak var score: UILabel!
-    @IBOutlet weak var seconds: UILabel!
-    @IBOutlet var lettersLabel: [UIButton]!
     
-
+    var scoreArr: [Score] = CoreDataManager.shared.fetchScores()
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let length = lettersLabel.count
-        
-        gameBrain.newGame(numLetters: length)
-        updateUI()
-        configureTimer()
-        
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        
-        timer.invalidate()
-    }
-    
-    func configureTimer() {
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: fireTimer(timer:))
-        RunLoop.current.add(timer, forMode: .common)
-    }
-    
-    func updateUI() {
-        targetLetter.text = gameBrain.targetLetter
-        let scoreText = String(gameBrain.score)
-        let secondsText = String(gameBrain.secondsRemaining)
-        
-        score.text = "Score:\(scoreText)"
-        seconds.text = "Seconds:\(secondsText)"
-        
-        for i in 0..<lettersLabel.count {
-            lettersLabel[i].setTitle( gameBrain.randomLetters[i], for: .normal)
-        }
-    }
+        tableView.dataSource = self
 
-    @IBAction func letterButtonTapped(_ sender: UIButton) {
-        
-        sender.backgroundColor = UIColor.systemRed
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-
-                    sender.backgroundColor = UIColor.systemBlue
-                }
-
-        
-        if let letterKey = sender.titleLabel?.text{
-            
-            gameBrain.letterSelected(letterPressed: letterKey)
-        }
-        
-        updateUI()
     }
     
     
-    func fireTimer(timer: Timer) {
-        gameBrain.secondsRemaining -= 1
-        updateUI()
+
+}
+
+extension ScoreViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        if gameBrain.secondsRemaining <= 0 {
-            timer.invalidate()
-            navigationController?.popToRootViewController(animated: true)
-        }
+        
+        return scoreArr.count
+        
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "scoreCell", for: indexPath)
+        
+        
+        cell.textLabel?.text = String(scoreArr[indexPath.row].score)
+        
+        
+        
+        return cell
+    }
+    
+    
 }
